303c8210b21a98f84526f72b453da31a67c3fefe
radjaho973 <adjaho.b@hotmail.fr>
2023-07-07 22:46:59 -0400

    Creation de l'entite utilisateur, ajout des roles, login et register

--- src/Controller/CarController.php
+++ src/Controller/CarController.php
@@ -10,7 +10,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('/car')]
+#[Route('/back-office/car')]
 class CarController extends AbstractController
 {
     #[Route('/', name: 'app_car_index', methods: ['GET'])]
