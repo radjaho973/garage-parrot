7e9da7f803507ab927ea8a9d6a7cd0c8b1b62afc
radjaho973 <adjaho.b@hotmail.fr>
2023-07-14 13:36:58 -0400

    ajout d'un rate limit au formulaire de recherche AJAX

--- src/Controller/CarIndexController.php
+++ src/Controller/CarIndexController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Form\CarType;
 use App\Form\CarSearchType;
 use App\Repository\CarRepository;
 use Symfony\Component\HttpFoundation\Request;
@@ -12,6 +13,7 @@ use Symfony\Component\RateLimiter\RateLimiterFactory;
 use Symfony\Component\Serializer\SerializerInterface;
 use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpKernel\Exception\TooManyRequestsHttpException;
 
 class CarIndexController extends AbstractController
 {
@@ -24,15 +26,6 @@ class CarIndexController extends AbstractController
         $form = $this->createForm(CarSearchType::class);
         $form->handleRequest($request);
 
-        if ($request->isXmlHttpRequest()) {
-            
-            function ajaxSearch($request){
-                $requestBody = $request->getContent();
-                return json_encode("ceci est une réponse de symfony");
-            }
-        }else{
-        }
-
         // if ($form->isSubmitted() && $form->isValid()) {
             // dd($form->get('recherche')->getData());
         // }
@@ -42,37 +35,45 @@ class CarIndexController extends AbstractController
             'controller_name' => 'CarIndexController',
         ]);
     }
+     // if you're using service autowiring, the variable name must be:
+    // "rate limiter name" (in camelCase) + "Limiter" suffix
     #[Route('/car/index/ajax', name: 'app_car_index_ajax', methods:["POST"])]
-    public function ajax(CarRepository $carRepo,Request $request,RateLimiterFactory $rateLimit, NormalizerInterface $normalizer): JsonResponse
+    public function ajax(CarRepository $carRepo,Request $request,RateLimiterFactory $ajaxRequestLimiter, NormalizerInterface $normalizer): JsonResponse
     {
         
         if (!$request->isXmlHttpRequest()) {
             return $this->json('error');
         }else{
             //ajout d'une limite de requête ajax pour sécuriser l'application
-            $limiter = $rateLimit->create($request->getClientIp());
-            // https://symfony.com/doc/current/rate_limiter.html#rate-limiting-in-action
-            //passe les classes à l'intérieur de la fonction pour les
-            //utiliser
-            function dbQuery($request, CarRepository $carRepo){
+            $limiter = $ajaxRequestLimiter->create($request->getClientIp());
+
+            if ($limiter->consume(1)->isAccepted() == true) {
                 
-                $data = json_decode($request->getContent(),true);
-                //requête personalisé dans le repo
-                $dbQueryResult = $carRepo->search($data["searchQuery"]);
-            
-                return $dbQueryResult;
-            }
-            // Rqresult est un tableau avec les résultats de la recherche
-            $Rqresult = dbQuery($request,$carRepo);
-            // options 'groups' défini dans les entités
-            // Car;Category;ImageCollection & Brand afin de signaler à
-            //normalizer quel données encodé en format Json pour éviter les boucles infinies.
-            $carNormalized = $normalizer->normalize($Rqresult,null,[
-                'groups' => 'car:object'
-            ]);
-        }   
-        //retourne une JsonResponse
-        return $this->json($carNormalized);
+                //passe les classes à l'intérieur de la fonction pour les
+                //utiliser
+                function dbQuery($request, CarRepository $carRepo){
+                    
+                    $data = json_decode($request->getContent(),true);
+                    //requête personalisé dans le repo
+                    $dbQueryResult = $carRepo->search($data["searchQuery"]);
+                    
+                    return $dbQueryResult;
+                }
+                // Rqresult est un tableau avec les résultats de la recherche
+                $Rqresult = dbQuery($request,$carRepo);
+                // options 'groups' défini dans les entités
+                // Car;Category;ImageCollection & Brand afin de signaler à
+                //normalizer quel données encodé en format Json pour éviter les boucles infinies.
+                $carNormalized = $normalizer->normalize($Rqresult,null,[
+                    'groups' => 'car:object'
+                ]);
+                //retourne une JsonResponse
+                // return $this->json($carNormalized);
+                return $this->json($carNormalized);
+            }else{  //si aucun token n'est disponible
+                throw new TooManyRequestsHttpException();
+            } 
+        }
        
     }
 }
--- src/Form/CarSearchType.php
+++ src/Form/CarSearchType.php
@@ -3,34 +3,76 @@
 namespace App\Form;
 
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
+use Symfony\Component\Validator\Constraints\File;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\RangeType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class CarSearchType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('recherche', TextType::class,[
-                'label' => "Champ de recherche"
-            ])
-            ->add('save', SubmitType::class,[
-                'label' => 'Rechercher',
+            ->add('text_search',TextType::class,[
+                'required' => false,
+                'label' => false,
+                'help' => 'Rentrer un nom ou une marque de véhicule',
+                'sanitize_html' => true,
                 'attr' => [
-                    'type' => 'button'
+                    'placeholder' => 'Recherche',
+                    'class' => 'ajax-form',
                 ]
             ])
-            // ->setMethod('GET')
-            ->getForm();
+            ->add('Prix',RangeType::class,[
+                
+                'attr' => [
+                    'min' => 0,
+                    'max' => 100000,
+                    'value' => 100000,
+                    // 'step' => 100
+                ],
+                'required' => false,
+                'invalid_message' => 'Rentrer un nombre ou un chiffre'
+            ])
+            ->add('yearPlacedInCirculation')
+            ->add('mileage')
+            ->add('description')
+            // ->add('categorie',TextType::class,[
+            //     'mapped'=> false
+            // ])
+            // ->add('Marque',TextType::class,[
+            //     'mapped'=> false
+            // ])
+            ->add('category', CollectionType::class,[
+                'entry_type'=> CategoryType::class,
+                    'by_reference'=> false,
+                    'allow_add'=> true,
+                    'allow_delete'=> true,
+                    'entry_options' => ['label'=>false],
+                    'prototype'=> true,
+            ])
+            // ->add('brand', CollectionType::class,[
+            //     'entry_type'=> BrandType::class,
+            //         'by_reference'=> false,
+            //         'allow_add'=> true,
+            //         'allow_delete'=> true,
+            //         'entry_options' => ['label'=>true],
+            //         'prototype'=> true,
+            // ])
+            // ->add('image_collection', CollectionType::class,[
+            //     'mapped' => false,
+            //     'entry_type'=> ImageCollectionType::class,
+            //         'by_reference'=> false,
+            //         'allow_add'=> true,
+            //         'allow_delete'=> true,
+            //         'entry_options' => ['label'=>false],
+            //         'prototype'=> true,
+            // ])
+            
         ;
-    }
-
-    public function configureOptions(OptionsResolver $resolver): void
-    {
-        $resolver->setDefaults([
-            // Configure your form options here
-        ]);
     }
 }
